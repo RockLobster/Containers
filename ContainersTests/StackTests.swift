8693cc61c47ab294f519d469468ed3bcd2760785
Rainer SchloÃànvoigt <rainer.schlonvoigt@philips.com>
2015-10-19 18:01:41 +0200

    Added more tests

--- ContainersTests/StackTests.swift
+++ ContainersTests/StackTests.swift
@@ -8,15 +8,29 @@ class StackTests: QuickSpec {
         
         describe("stack") {
             
-            let stack = Stack()
+            var stack: Stack!
+            
+            beforeEach({ () -> () in
+                stack = Stack()
+            })
             
             it("can be created") {
                 expect(stack).toNot(beNil())
+            }
+            
+            it("is empty after creation") {
+                expect(stack.count).to(equal(0))
             }
 
             it("can put a container on top") {
                 stack.push(0)
                 expect(stack.count).to(equal(1))
+            }
+            
+            it("can put multiple containers on top") {
+                stack.push(2)
+                stack.push(3)
+                expect(stack.count).to(equal(2))
             }
         }
     }
